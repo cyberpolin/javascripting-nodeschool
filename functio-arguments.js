9acab96ca80942ccc011263c385b8fd5b43e5c54
Carlos Gonzalez <cyberpolin@gmail.com>
2016-07-18 17:57:37 -0500

    Add finished nodeschool javascripting workshop

---
+++ functio-arguments.js
@@ -0,0 +1,4 @@
+let math = function(one, two, three){
+  return (two*three)+one;
+}
+console.log(math(53, 61, 67));
---
+++ functions.js
@@ -0,0 +1,6 @@
+let eat = function(food){
+  return food + ' tasted really good.';
+
+}
+
+console.log(eat('bananas'));
